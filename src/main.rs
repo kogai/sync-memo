c333c16d8b094698a5753336529d3de9a8a73dba
小飼慎一 <kogai0121@gmail.com>
2017-05-05 20:14:22 +0900

    fmt

--- src/main.rs
+++ src/main.rs
@@ -20,47 +20,39 @@ use std::env;
 use clap::{App, Arg, SubCommand};
 
 fn main() {
-    let path = env::home_dir().and_then(|x| Some(x.join("sync-memo").join(".sync-memo-config.json"))).unwrap();
+    let path = env::home_dir()
+        .and_then(|x| Some(x.join("sync-memo").join(".sync-memo-config.json")))
+        .unwrap();
     let file_handler = handler::FileHandler::new(path);
 
     let matches = App::new("sync-memo")
         .version("0.0.1")
         .about("sync local memo files via gist")
-        .subcommand(
-            SubCommand::with_name("add")
+        .subcommand(SubCommand::with_name("add")
             .about("add file to sync")
-            .arg(
-                Arg::with_name("files")
+            .arg(Arg::with_name("files")
                 .required(true)
-                .multiple(true)
-            )
-        )
-        .subcommand(
-            SubCommand::with_name("watch")
-            .about("watch files to sync")
-        )   
-        .subcommand(
-            SubCommand::with_name("show")
-            .about("show all gists")
-        )
+                .multiple(true)))
+        .subcommand(SubCommand::with_name("watch").about("watch files to sync"))
+        .subcommand(SubCommand::with_name("show").about("show all gists"))
         .get_matches();
-    
+
     if let Some(m) = matches.subcommand_matches("add") {
         let file_names = values_t!(m.values_of("files"), String).unwrap();
         for file_name in file_names {
             file_handler.add_files(file_name);
         }
     }
-    
+
     if let Some(_) = matches.subcommand_matches("watch") {
         let handlers = file_handler.watch_all_files();
         for h in handlers {
             println!("watch thread deliminated with {:?}", h.join());
-        };
+        }
     }
-         
+
     if let Some(_) = matches.subcommand_matches("show") {
         let gists = github::get_gist("");
         println!("{:?}", gists);
     }
-} 
+}
--- src/watcher.rs
+++ src/watcher.rs
@@ -8,7 +8,9 @@ use github;
 
 const INTERVAL: u64 = 10;
 
-pub fn watch(path: String, gist_id: &str, (tx, rx): (Sender<DebouncedEvent>, Receiver<DebouncedEvent>)) {
+pub fn watch(path: String,
+             gist_id: &str,
+             (tx, rx): (Sender<DebouncedEvent>, Receiver<DebouncedEvent>)) {
     let mut watcher = watcher(tx, Duration::from_secs(INTERVAL)).unwrap();
     let path_to_file = path.as_str();
     watcher.watch(path_to_file, RecursiveMode::Recursive).unwrap();
@@ -19,17 +21,14 @@ pub fn watch(path: String, gist_id: &str, (tx, rx): (Sender<DebouncedEvent>, Rec
                 let mut file = File::open(path_to_file).unwrap();
                 let mut contents = String::new();
                 file.read_to_string(&mut contents).unwrap();
-                let gist_modified = github::modify_gist(gist_id,
-                                                        path.clone(),
-                                                        contents);
+                let gist_modified = github::modify_gist(gist_id, path.clone(), contents);
                 println!("gist modified {:?}", gist_modified);
             }
             Err(e) => {
                 println!("watch error: {:?}", e);
                 watcher.unwatch(&path).unwrap();
                 break;
-            },
+            }
         }
     }
 }
-
